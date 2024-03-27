d4adbe709499fe858332aa7226d1c4565f28ee6f
chitraw <chitraameneni03@gmail.com>
2024-03-27 23:41:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Selectable_grid from "./Components/Selectable_grid";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h1>Selectable Grid</h1>
+        <Selectable_grid rows={10} cols={10} />
+        {/*
+        My Interview Prep Course - https://roadsidecoder.com/course-details 
+      */}
+      </div>
     </div>
   );
 }
